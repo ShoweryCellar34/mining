9493501ef4cd1e05c2612ba60acd70e3e547e616
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2023-07-08 13:14:50 +1200

    Update start_miner.sh

--- PhoenixMiner_6.2c_Linux/start_miner.sh
+++ PhoenixMiner_6.2c_Linux/start_miner.sh
@@ -3,4 +3,4 @@
 #
 
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./PhoenixMiner -pool ssl://eu1.ethermine.org:5555 -pool2 ssl://us1.ethermine.org:5555 -wal 0x008c26f3a2Ca8bdC11e5891e0278c9436B6F5d1E.Rig001
+./PhoenixMiner -pool ssl://eu1.ethermine.org:5555 -pool2 ssl://us1.ethermine.org:5555 -config config.txt
