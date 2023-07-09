da05ccd275f06550e86555e03890b3e34e36588c
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2023-07-09 15:51:48 +1200

    Add files via upload

---
+++ PhoenixMiner_6.2c_Linux_configured/start_miner.sh
@@ -0,0 +1,7 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+sudo ./PhoenixMiner -config config.txt #-pool ssl://eu1.ethermine.org:5555 -pool2 ssl://us1.ethermine.org:5555 -config config.txt
+pause
---
+++ PhoenixMiner_6.2c_Linux_configured/start_miner_etc.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETC
+#
+
+# IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
+./PhoenixMiner -pool ssl://eu1-etc.ethermine.org:5555 -wal 0xd4BeCA2987aa0C09916c9F370dDac0655751b078 -worker ShoweryCellar34 -coin etc -mclock +400 -cclock +400
